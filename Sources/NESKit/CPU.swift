3eb590734360fac40f0c7af4052a12ffa649bf4b
Helloyunho <yunho050840@gmail.com>
2022-01-02 16:07:02 +0900

    ✨ feat: implement allocation

--- Sources/NESKit/CPU.swift
+++ Sources/NESKit/CPU.swift
@@ -3,9 +3,12 @@ struct CPU6502 {
         case A, X, Y, S, P
     }
 
-    var registers: [RegisterKeys: UInt8] = [:]
+    private var registers: [RegisterKeys: UInt8] = [:]
     private var _PC: UInt16 = 0
-    var PC: UInt16 {
+    private var allocs: [UInt8] = Array(repeating: 0, count:0xffff)
+    // Separated since PC needs 16 bits.
+    /// Program counter register.
+    public var PC: UInt16 {
         get {
             return _PC
         }
@@ -19,7 +22,7 @@ struct CPU6502 {
     }
 
     /**
-     Gets register by register key.
+     Gets register value by register key.
      - parameters:
         - register: Register key you want to get the value.
      - returns: Register value matches with the key.
@@ -36,6 +39,51 @@ struct CPU6502 {
             }
         }
     }
+    
+    /**
+     Gets allocated value by index.
+     - parameters:
+        - index: Index of the part you want to get.
+     - returns: Allocated value matches with the index.
+     */
+    subscript(index: Int) -> UInt8 {
+        get {
+            return allocs[index]
+        }
+        set(value) {
+            allocs[index] = value
+        }
+    }
+    
+    /**
+     Gets all allocated values in the provided range.
+     - parameters:
+        - bounds: Index range of the parts you want to get.
+     - returns: Allocated values in the parameter range.
+     */
+    subscript(bounds: Range<Int>) -> ArraySlice<UInt8> {
+        get {
+            return allocs[bounds]
+        }
+        set(value) {
+            allocs[bounds] = value
+        }
+    }
+    
+    /**
+     Gets all allocated values in the provided range.
+     - parameters:
+        - r: Index range of the parts you want to get.
+     - returns: Allocated values in the parameter range.
+     */
+    subscript<R>(r: R) -> ArraySlice<UInt8> where R : RangeExpression, Int == R.Bound {
+        get {
+            return allocs[r]
+        }
+        set(value) {
+            allocs[r] = value
+        }
+    }
 
     /**
      Resets all the registers.
@@ -48,7 +96,7 @@ struct CPU6502 {
             .S: 0,
             .P: 0,
         ]
-        PC = 0
+        PC = readAllocU16(index: 0xFFFC)
     }
     
     /**
@@ -98,17 +146,56 @@ struct CPU6502 {
             self[.P] &= 0b0111_1111
         }
     }
-
+    
     /**
-     Interprets the program passed by the argument.
-     Check the P register to check the status of CPU.
+     Allocates the program into CPU.
      - parameters:
         - program: Program code in 6502 machine language.
      */
-    public mutating func interpret(program: [UInt8]) {
+    public mutating func load(program: [UInt8]) {
+        allocs.replaceSubrange(0x8000 ..< (0x8000 + program.count), with: program)
+        writeAllocU16(index: 0xFFFC, value: 0x8000)
+    }
+    
+    /**
+     Reads 2 bytes(16 bits) from the provided index.
+     - parameters:
+        - index: Index value you want to start from.
+     */
+    public func readAllocU16(index: Int) -> UInt16 {
+        // Somehow UInt16 by default reads bytes in little endian order
+        // So we don't have to do anything yayy
+        // Code from: https://stackoverflow.com/a/47764694/9376340
+        return self[index ... index + 1].withUnsafeBytes { $0.load(as: UInt16.self) }
+    }
+    
+    /**
+     Writes 2 bytes(16 bits) from the provided index.
+     - parameters:
+        - index: Index value you want to start from.
+        - value: 16 bits value you want to write.
+     */
+    public mutating func writeAllocU16(index: Int, value: UInt16) {
+        // It wasa easy to convert array to uint16
+        // But hard to convert uint16 back into array oof
+        // Code from: https://gist.github.com/kimjj81/aadf55fc591220afdc8450452c2ea21d
+        var _endian = value.littleEndian
+        let bytePtr = withUnsafePointer(to: &_endian) {
+            $0.withMemoryRebound(to: UInt8.self, capacity: MemoryLayout<UInt16>.size) {
+                UnsafeBufferPointer(start: $0, count: MemoryLayout<UInt16>.size)
+            }
+        }
+        allocs.replaceSubrange(index ... (index + 1), with: [UInt8](bytePtr))
+    }
+
+    /**
+     Executes the program allocated on CPU's allocations.
+     Check the P register to check the status of CPU.
+     */
+    public mutating func run() {
         reset()
         while true {
-            let opcode = program[Int(PC)]
+            let opcode = self[Int(PC)]
             PC &+= 1
 
             switch opcode {
@@ -117,8 +204,8 @@ struct CPU6502 {
                 return
             case 0xa9:
                 // LDA #$nn (immediate)
-                let param = program[Int(PC)]
-                PC += 1
+                let param = self[Int(PC)]
+                PC &+= 1
                 let result = LDA(value: param)
                 updateStatus(result: result)
             case 0xaa:
@@ -132,5 +219,15 @@ struct CPU6502 {
                 break
             }
         }
+    }
+    
+    /**
+     Allocates the program into CPU and executes the program.
+     - parameters:
+        - program: Program code in 6502 machine language.
+     */
+    public mutating func loadAndRun(program: [UInt8]) {
+        self.load(program: program)
+        self.run()
     }
 }
--- Tests/NESKitTests/CPUTests.swift
+++ Tests/NESKitTests/CPUTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class CPUTests: XCTestCase {
     func testLDAImmidiateLoadData() {
         var cpu = CPU6502()
-        cpu.interpret(program: [0xa9, 0x05, 0x00])
+        cpu.loadAndRun(program: [0xa9, 0x05, 0x00])
         XCTAssertEqual(cpu[.A], 0x05)
         XCTAssertTrue(cpu[.P] & 0x2 == 0)
         XCTAssertTrue(cpu[.P] & 0x80 == 0)
@@ -12,25 +12,25 @@ final class CPUTests: XCTestCase {
     
     func testLDAZeroFlag() {
         var cpu = CPU6502()
-        cpu.interpret(program: [0xa9, 0x00, 0x00])
+        cpu.loadAndRun(program: [0xa9, 0x00, 0x00])
         XCTAssertTrue(cpu[.P] & 0x2 == 2)
     }
     
     func testTAX() {
         var cpu = CPU6502()
-        cpu.interpret(program: [0xa9, 0x05, 0xaa, 0x00])
+        cpu.loadAndRun(program: [0xa9, 0x05, 0xaa, 0x00])
         XCTAssertEqual(cpu[.X], cpu[.A])
     }
     
     func testINX() {
         var cpu = CPU6502()
-        cpu.interpret(program: [0xe8, 0x00])
+        cpu.loadAndRun(program: [0xe8, 0x00])
         XCTAssertEqual(cpu[.X], 1)
     }
     
     func testINXOverflow() {
         var cpu = CPU6502()
-        cpu.interpret(program: [0xa9, 0xff, 0xaa, 0xe8, 0x00])
+        cpu.loadAndRun(program: [0xa9, 0xff, 0xaa, 0xe8, 0x00])
         XCTAssertEqual(cpu[.X], 0)
     }
 }
